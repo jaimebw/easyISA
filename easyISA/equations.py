08e8e881ed0abc6d185c700fb4ab553a35050645
Jaime Bowen <jaime@MacBook-Pro-de-Jaime.local>
2020-12-07 03:21:32 +0100

    init commit

---
+++ easyISA/equations.py
@@ -0,0 +1,44 @@
+import math
+
+
+t0 = 288.15
+p0 = 101325
+p11 = 226.32e3
+t11 = 216.65
+R = 287
+g = 9.81
+
+
+class temperature:
+
+    @staticmethod
+    def as_meters(meters):
+        if meters > 11000:
+            return 216.65
+        else:
+            return t0 - 6.5*meters/1000
+
+    @staticmethod
+    def as_feet(feet):
+        if feet > 36089:
+            return 216.65
+        else:
+            return t0 -1.98*feet/1000
+
+class presure:
+
+    @staticmethod
+    def as_meters(meters):
+        if meters > 11000:
+            
+            return p11* math.exp(-g/(R*t11)*(meters-11000))
+        else:
+            return p0*(1-0.0065*meters/t0)**5.2561
+
+class density:
+
+    @staticmethod
+    def as_meters(meters):
+        density = presure.as_meters(meters)/(R * temperature.as_meters(meters))
+        return density
+   
