35356918e73aac84f4a17bee96c8b5c26e85e8a5
Jaime Bowen <jaime@MacBook-Pro-de-Jaime.local>
2020-12-08 22:11:19 +0100

    init commit

---
+++ tests/test_isa.py
@@ -0,0 +1,6 @@
+from easyISA.equations import temperature,density,presure
+
+def test_isa():
+    assert temperature.as_meters(meters=0) == 288.15
+    assert density.as_meters(meters=0) == 1.225
+    assert presure.as_meters(meters=0) == 101325
