db276058be4a9ebe9641fa22772b01297e1fd192
Jaime Bowen <jaime@MacBook-Pro-de-Jaime.local>
2020-12-08 22:14:18 +0100

    Arreglado test

--- tests/test_isa.py
+++ tests/test_isa.py
@@ -2,5 +2,5 @@ from easyISA.equations import temperature,density,presure
 
 def test_isa():
     assert temperature.as_meters(meters=0) == 288.15
-    assert density.as_meters(meters=0) == 1.225
+    assert (density.as_meters(meters=0) > 1.225)& (density.as_meters(meters=0) < 1.226)
     assert presure.as_meters(meters=0) == 101325
