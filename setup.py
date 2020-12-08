25e5a94f0deb8d427994a2f3a60bf8e680994307
Jaime Bowen <jaime@MacBook-Pro-de-Jaime.local>
2020-12-08 22:07:01 +0100

    init commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'easyISA',
+    packages=find_packages(include=['easyISA']),
+    version= '0.1.0',
+    description='Easy implementation of ISA equations',
+    author='Jaime Bowen',
+    install_requires = [],
+    license='MIT'
+)
