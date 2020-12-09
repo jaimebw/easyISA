2aa30395e860e2ef270933838ad1779e20e6ae06
Jaime Bowen <jaime@MacBook-Pro-de-Jaime.local>
2020-12-09 23:17:37 +0100

    Black formatting

--- easyISA/equations.py
+++ easyISA/equations.py
@@ -1,6 +1,6 @@
 import math
 
-# Constants
+#  Constants
 t0 = 288.15
 p0 = 101325
 p11 = 226.32e2
@@ -10,7 +10,6 @@ g = 9.81
 
 
 class temperature:
-
     @staticmethod
     def meters(altitude):
         """
@@ -19,7 +18,7 @@ class temperature:
         if altitude > 11000:
             return 216.65
         else:
-            return t0 - 6.5*altitude/1000
+            return t0 - 6.5 * altitude / 1000
 
     @staticmethod
     def feet(feet):
@@ -29,29 +28,27 @@ class temperature:
         if altitude > 36089:
             return 216.65
         else:
-            return t0 -1.98*altitude/1000
+            return t0 - 1.98 * altitude / 1000
+
 
 class presure:
-
     @staticmethod
     def meters(altitude):
         """
         Returns presure in Pa for height in METERS
         """
         if altitude > 11000:
-            
-            return p11* math.exp(-g/(R*t11)*(altitude-11000))
+
+            return p11 * math.exp(-g / (R * t11) * (altitude - 11000))
         else:
-            return p0*(1-0.0065*altitude/t0)**5.2561
+            return p0 * (1 - 0.0065 * altitude / t0) ** 5.2561
+
 
 class density:
-    
-
     @staticmethod
     def meters(altitude):
         """
         Returns density in kg/m^3 for height in altitude
         """
-        density = presure.meters(altitude)/(R * temperature.meters(altitude))
+        density = presure.meters(altitude) / (R * temperature.meters(altitude))
         return density
-   
