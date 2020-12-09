67c20d1f44dfc15dc2a2e89c2350c2fe9e072189
Jaime Bowen <jaime@MacBook-Pro-de-Jaime.local>
2020-12-09 23:15:52 +0100

    Improved testing, change in format

--- tests/test_isa.py
+++ tests/test_isa.py
@@ -1,6 +1,10 @@
 from easyISA.equations import temperature,density,presure
 
-def test_isa():
-    assert temperature.as_meters(meters=0) == 288.15
-    assert (density.as_meters(meters=0) > 1.225)& (density.as_meters(meters=0) < 1.226)
-    assert presure.as_meters(meters=0) == 101325
+def test_equations():
+    tol = 1e-3
+    assert temperature.meters(altitude=0) == 288.15
+    assert (density.meters(altitude=0) > 1.225)& (density.meters(altitude=0) < 1.226)
+    assert presure.meters(altitude=0) == 101325
+    assert round(temperature.meters(altitude=11000),2) == 216.65
+    diff_p = abs((abs(presure.meters(altitude=11000)) - 226.32e2)/presure.meters(altitude=11000))
+    assert abs(tol) - diff_p > 0
