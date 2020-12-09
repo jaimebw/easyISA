d93b03d137b28757b4150a5b58944615908f75b3
Jaime Bowen <jaime@MacBook-Pro-de-Jaime.local>
2020-12-09 23:16:02 +0100

    Change in the naming

--- easyISA/equations.py
+++ easyISA/equations.py
@@ -3,7 +3,7 @@ import math
 # Constants
 t0 = 288.15
 p0 = 101325
-p11 = 226.32e3
+p11 = 226.32e2
 t11 = 216.65
 R = 287
 g = 9.81
@@ -12,46 +12,46 @@ g = 9.81
 class temperature:
 
     @staticmethod
-    def as_meters(meters):
+    def meters(altitude):
         """
-        Returns temperature in Kelvin for height in METERS
+        Returns temperature in Kelvin for height in altitude
         """
-        if meters > 11000:
+        if altitude > 11000:
             return 216.65
         else:
-            return t0 - 6.5*meters/1000
+            return t0 - 6.5*altitude/1000
 
     @staticmethod
-    def as_feet(feet):
+    def feet(feet):
         """
         Returns temperature in Kelvin for height in FEETS
         """
-        if feet > 36089:
+        if altitude > 36089:
             return 216.65
         else:
-            return t0 -1.98*feet/1000
+            return t0 -1.98*altitude/1000
 
 class presure:
 
     @staticmethod
-    def as_meters(meters):
+    def meters(altitude):
         """
         Returns presure in Pa for height in METERS
         """
-        if meters > 11000:
+        if altitude > 11000:
             
-            return p11* math.exp(-g/(R*t11)*(meters-11000))
+            return p11* math.exp(-g/(R*t11)*(altitude-11000))
         else:
-            return p0*(1-0.0065*meters/t0)**5.2561
+            return p0*(1-0.0065*altitude/t0)**5.2561
 
 class density:
     
 
     @staticmethod
-    def as_meters(meters):
+    def meters(altitude):
         """
-        Returns density in kg/m^3 for height in METERS
+        Returns density in kg/m^3 for height in altitude
         """
-        density = presure.as_meters(meters)/(R * temperature.as_meters(meters))
+        density = presure.meters(altitude)/(R * temperature.meters(altitude))
         return density
    
