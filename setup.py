a88ed439616023c5b639e8a4301eb5208cb0711d
Jaime Bowen <jaime@MacBook-Pro-de-Jaime.local>
2020-12-12 15:36:58 +0100

    New format

--- setup.py
+++ setup.py
@@ -1,14 +1,32 @@
 from setuptools import find_packages, setup
 
 setup(
-    name = 'easyISA',
+    name = 'easyISA-jaimebw',
     packages=find_packages(include=['easyISA']),
     version= '0.1.0',
     description='Easy implementation of ISA equations',
     author='Jaime Bowen',
+    author_email="jaimebwv@gmail.com",
     install_requires = [],
     license='MIT',
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     test_suite='tests',
+    url="https://github.com/jaimebw/easyISA",
+    python_requires='>=3.6'
 )
+"""
+setuptools.setup(
+    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
+    
+  
+    
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    
+)
+"""
