effe555befc33bb83de7632cbb36d83b626fc277
jaimebw <jaimebwv@gmail.com>
2021-03-21 23:30:52 +0100

    Arrelgados los tests

--- tests/test_isa.py
+++ tests/test_isa.py
@@ -1,10 +1,10 @@
-from easyISA.equations import temperature,density,presure
+from easyISA.equations import temperature,density,pressure
 
 def test_equations():
     tol = 1e-3
     assert temperature.meters(altitude=0) == 288.15
     assert (density.meters(altitude=0) > 1.225)& (density.meters(altitude=0) < 1.226)
-    assert presure.meters(altitude=0) == 101325
+    assert pressure.meters(altitude=0) == 101325
     assert round(temperature.meters(altitude=11000),2) == 216.65
-    diff_p = abs((abs(presure.meters(altitude=11000)) - 226.32e2)/presure.meters(altitude=11000))
+    diff_p = abs((abs(pressure.meters(altitude=11000)) - 226.32e2)/pressure.meters(altitude=11000))
     assert abs(tol) - diff_p > 0
